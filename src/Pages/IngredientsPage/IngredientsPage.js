7d9631fe8477763e74e922d5cbacf10044dac699
DilanAtomica <dilan-shwane@hotmail.com>
2022-08-16 15:10:14 +0200

    Added a function to click on searched ingredients to add them to your "cart"

--- src/Pages/IngredientsPage/IngredientsPage.js
+++ src/Pages/IngredientsPage/IngredientsPage.js
@@ -8,15 +8,31 @@ function IngredientsPage(props) {
 
     const [searchedIngredients, setSearchedIngredients] = useState([]);
 
+    const [addedIngredients, setAddedIngredients] = useState([]);
+
+    const [searchInput, setSearchInput] = useState("");
+
     const getIngredient = async(e) => {
+        setSearchInput(e.target.value);
         if(e.target.value.length > 2) {
             try {
                 const API = "https://api.spoonacular.com/food/ingredients/autocomplete?query=" + e.target.value
                     + "&apiKey=19cbc6a6b21d4037815a9a3a15f7d294"
-                console.log(API);
                 const response = await axios.get(API);
                 const results = response.data;
-                setSearchedIngredients(results);
+
+                let newList = [];
+
+                for(let i = 0; i < results.length - 1; i++) {
+                    newList.push(results[i]);
+                    for(let j = 0; j <= addedIngredients.length - 1; j++) {
+                            newList = newList.filter((ingredient) => {
+                                return ingredient.name !== addedIngredients[j].name
+                            })
+                    }
+                }
+
+                setSearchedIngredients(newList);
             } catch {
                 console.log("Error")
             }
@@ -25,14 +41,32 @@ function IngredientsPage(props) {
         }
     }
 
+    const addIngredient = (e) => {
+        setSearchInput("");
+        setSearchedIngredients([]);
+        const ingredientName = e.target.getAttribute("data-name");
+        const ingredientImage = e.target.getAttribute("data-image");
+        if(ingredientName === null) return
+        console.log(ingredientName);
+
+        setAddedIngredients([...addedIngredients,
+             {
+                name: ingredientName,
+                image: ingredientImage
+            }
+        ])
+    }
+
     return (
         <div className="ingredientsPage">
             <div className="ingredientsContainer">
                 <div className="searchContainer">
-                    <input onChange={getIngredient} type="text" />
+                    <input value={searchInput} onChange={getIngredient} type="text" />
                     <div className="autoCompletedIngredients">
                         {searchedIngredients?.map(ingredient => (
-                            <button className="addIngredientButton">
+                            <button data-name={ingredient.name}
+                                    data-image={"https://spoonacular.com/cdn/ingredients_100x100/" + ingredient.image}
+                                    onClick={addIngredient} key={ingredient.name} className="addIngredientButton">
                                 <span className="addIngredientIcon"><AiOutlinePlus /></span>{ingredient.name}
                                 <img src={"https://spoonacular.com/cdn/ingredients_100x100/" + ingredient.image} />
                             </button>
@@ -40,19 +74,14 @@ function IngredientsPage(props) {
                     </div>
                 </div>
                 <div className="addedIngredients">
-                    <div className="ingredient">
-                        <img src="https://spoonacular.com/cdn/ingredients_100x100/apple.jpg" />
-                        <div className="ingredientTag">
-                            Apple<button type="button" className="deleteIcon"><TiDelete /></button>
+                    {addedIngredients.map(ingredient => (
+                        <div key={ingredient.name} className="ingredient">
+                            <img src={ingredient.image} />
+                            <div className="ingredientTag">
+                                {ingredient.name}<button type="button" className="deleteIcon"><TiDelete /></button>
+                            </div>
                         </div>
-                    </div>
-
-                    <div className="ingredient">
-                        <img src="https://spoonacular.com/cdn/ingredients_100x100/apple.jpg" />
-                        <div className="ingredientTag">
-                            Apple<button type="button" className="deleteIcon"><TiDelete /></button>
-                        </div>
-                    </div>
+                    ))}
 
                 </div>
             </div>
