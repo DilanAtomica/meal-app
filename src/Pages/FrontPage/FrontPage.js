e8b0438bc3ccf8d648395d11ed87591c84482efe
DilanAtomica <dilan-shwane@hotmail.com>
2022-08-16 10:22:08 +0200

    Added the functions to navigate to the ingridient and nutrient site

--- src/Pages/FrontPage/FrontPage.js
+++ src/Pages/FrontPage/FrontPage.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import {useNavigate} from "react-router-dom";
 import "./FrontPage.css";
 
 function FrontPage(props) {
 
+    let navigate = useNavigate();
+
     const handleOnClick = (e) => {
         if(e.target.innerHTML === "Ingredients") {
-
+            navigate("/ingredients/");
         } else {
-
+            navigate("/nutrients/");
         }
     }
 
